04e3e278cdab3037ba9308933723aa6625fa7faf
TerrieL <60408333+terrielama@users.noreply.github.com>
2024-04-04 11:10:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,18 +6,6 @@ const bodyColor = document.querySelector("body");
 let index = 0
 const maxIndex = 2
 
-// ---------------Changer la couleur du fond (body)----------------------------
-
-const setUi = () => {
-    slidesContainer.style.transform = `translateX(-${index * 100}%)`;
-
-    if (index == 0) {
-        bodyColor.style.backgroundColor = `#242830`;
-    } else if (index == 1) {
-        bodyColor.style.backgroundColor = `#f5f7f8`;
-    } else if (index == 2) {
-        bodyColor.style.backgroundColor = `#000`;
-    }
 
 //----------------Afficher ou cacher les boutons----------------
 
@@ -28,8 +16,7 @@ const setUi = () => {
     }
     if (index == 1) {
         previousButton.style.visibility = `visible`;
-    } 
-    
+    }   
     if (index == 2) {
         nextButton.style.visibility = `hidden`;
     } else {
@@ -48,7 +35,19 @@ nextButton.addEventListener("click", () => {
     setUi();
 });
   
-  
+// ---------------Changer la couleur du fond (body)----------------------------
+
+const setUi = () => {
+    slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+
+    if (index == 0) {
+        bodyColor.style.backgroundColor = `#242830`;
+    } else if (index == 1) {
+        bodyColor.style.backgroundColor = `#f5f7f8`;
+    } else if (index == 2) {
+        bodyColor.style.backgroundColor = `#000`;
+    }
+
   
   
   
